1adacb54c15a356f4b580bfb711f0f1b79f325c7
Mondit Thumniramon <mondit.thum@gmail.com>
2018-04-16 14:12:09 +0700

    Add real props for my profile page

--- app/containers/MyProfile/index.js
+++ app/containers/MyProfile/index.js
@@ -27,135 +27,136 @@ const actionToProps = {}
 @withRedux(mapStateToProps, actionToProps)
 export default class extends React.Component {
   static defaultProps = {
-    favorites: [
-      {
-        label: 'FAVORITES',
-        subLabel: '',
-        color: '#ff7547',
-        medias: [
-          {
-            title: '“I’m also just a girl…”',
-            subTitle: 'Notting Hills',
-            timing: '0.14 sec',
-            img: 'http://via.placeholder.com/246x369'
-          },
-          {
-            title: 'You’re waiting for a train',
-            subTitle: 'The Dark Knight',
-            timing: '0.14 sec',
-            img: 'http://via.placeholder.com/246x369'
-          },
-          {
-            title: 'You’re waiting for a train',
-            subTitle: 'The Dark Knight',
-            timing: '0.14 sec',
-            img: 'http://via.placeholder.com/246x369'
-          },
-          {
-            title: 'You’re waiting for a train',
-            subTitle: 'The Dark Knight',
-            timing: '0.14 sec',
-            img: 'http://via.placeholder.com/246x369'
-          },
-          {
-            title: 'You’re waiting for a train',
-            subTitle: 'The Dark Knight',
-            timing: '0.14 sec',
-            img: 'http://via.placeholder.com/246x369'
-          },
-          {
-            title: 'You’re waiting for a train',
-            subTitle: 'The Dark Knight',
-            timing: '0.14 sec',
-            img: 'http://via.placeholder.com/246x369'
-          },
-          {
-            title: 'You’re waiting for a train',
-            subTitle: 'The Dark Knight',
-            timing: '0.14 sec',
-            img: 'http://via.placeholder.com/246x369'
-          }
-        ]
-      }
-    ],
-    dubs: [
-      {
-        label: 'MY DUBS',
-        subLabel: '',
-        color: '#ff7547',
-        medias: [
-          {
-            title: '“I’m also just a girl…”',
-            subTitle: 'Notting Hills',
-            timing: '0.14 sec',
-            img: 'http://via.placeholder.com/246x369'
-          },
-          {
-            title: 'You’re waiting for a train',
-            subTitle: 'The Dark Knight',
-            timing: '0.14 sec',
-            img: 'http://via.placeholder.com/246x369'
-          },
-          {
-            title: 'You’re waiting for a train',
-            subTitle: 'The Dark Knight',
-            timing: '0.14 sec',
-            img: 'http://via.placeholder.com/246x369'
-          },
-          {
-            title: 'You’re waiting for a train',
-            subTitle: 'The Dark Knight',
-            timing: '0.14 sec',
-            img: 'http://via.placeholder.com/246x369'
-          },
-          {
-            title: 'You’re waiting for a train',
-            subTitle: 'The Dark Knight',
-            timing: '0.14 sec',
-            img: 'http://via.placeholder.com/246x369'
-          },
-          {
-            title: 'You’re waiting for a train',
-            subTitle: 'The Dark Knight',
-            timing: '0.14 sec',
-            img: 'http://via.placeholder.com/246x369'
-          },
-          {
-            title: 'You’re waiting for a train',
-            subTitle: 'The Dark Knight',
-            timing: '0.14 sec',
-            img: 'http://via.placeholder.com/246x369'
-          }
-        ]
-      }
-    ]
+    user: {
+      name: 'Irene Indravudh',
+      profileImage: 'http://cdn02.cdn.justjaredjr.com/wp-content/uploads/headlines/2017/07/hp-jk-rowling-birthday-tweets.jpg'
+    },
+    favorites: {
+      label: 'FAVORITES',
+      subLabel: '',
+      color: '#ff7547',
+      medias: [
+        {
+          title: '“I’m also just a girl…”',
+          subTitle: 'Notting Hills',
+          timing: '0.14 sec',
+          img: 'http://via.placeholder.com/246x369'
+        },
+        {
+          title: 'You’re waiting for a train',
+          subTitle: 'The Dark Knight',
+          timing: '0.14 sec',
+          img: 'http://via.placeholder.com/246x369'
+        },
+        {
+          title: 'You’re waiting for a train',
+          subTitle: 'The Dark Knight',
+          timing: '0.14 sec',
+          img: 'http://via.placeholder.com/246x369'
+        },
+        {
+          title: 'You’re waiting for a train',
+          subTitle: 'The Dark Knight',
+          timing: '0.14 sec',
+          img: 'http://via.placeholder.com/246x369'
+        },
+        {
+          title: 'You’re waiting for a train',
+          subTitle: 'The Dark Knight',
+          timing: '0.14 sec',
+          img: 'http://via.placeholder.com/246x369'
+        },
+        {
+          title: 'You’re waiting for a train',
+          subTitle: 'The Dark Knight',
+          timing: '0.14 sec',
+          img: 'http://via.placeholder.com/246x369'
+        },
+        {
+          title: 'You’re waiting for a train',
+          subTitle: 'The Dark Knight',
+          timing: '0.14 sec',
+          img: 'http://via.placeholder.com/246x369'
+        }
+      ]
+    },
+    dubs: {
+      label: 'MY DUBS',
+      subLabel: '',
+      color: '#ff7547',
+      medias: [
+        {
+          title: '“I’m also just a girl…”',
+          subTitle: 'Notting Hills',
+          timing: '0.14 sec',
+          img: 'http://via.placeholder.com/246x369'
+        },
+        {
+          title: 'You’re waiting for a train',
+          subTitle: 'The Dark Knight',
+          timing: '0.14 sec',
+          img: 'http://via.placeholder.com/246x369'
+        },
+        {
+          title: 'You’re waiting for a train',
+          subTitle: 'The Dark Knight',
+          timing: '0.14 sec',
+          img: 'http://via.placeholder.com/246x369'
+        },
+        {
+          title: 'You’re waiting for a train',
+          subTitle: 'The Dark Knight',
+          timing: '0.14 sec',
+          img: 'http://via.placeholder.com/246x369'
+        },
+        {
+          title: 'You’re waiting for a train',
+          subTitle: 'The Dark Knight',
+          timing: '0.14 sec',
+          img: 'http://via.placeholder.com/246x369'
+        },
+        {
+          title: 'You’re waiting for a train',
+          subTitle: 'The Dark Knight',
+          timing: '0.14 sec',
+          img: 'http://via.placeholder.com/246x369'
+        },
+        {
+          title: 'You’re waiting for a train',
+          subTitle: 'The Dark Knight',
+          timing: '0.14 sec',
+          img: 'http://via.placeholder.com/246x369'
+        }
+      ]
+    }
   }
   render() {
-    const { favorites, dubs } = this.props
+    const { user, favorites, dubs } = this.props
+    console.log(this)
     return (
       <div className='page-myProfile'>
         <div className='container'>
           <div className='profile-panel'>
             <div className='title'>
               <span className='greeting'>Hello</span>,{' '}
-              <span className='name'>Irene Indravudh</span>
+              <span className='name'>{user.name || ''}</span>
             </div>
             <div className='panel'>
               <div className='profile-image-cropper'>
-                <img className='' src='http://via.placeholder.com/250x250' />
+                <img className='' src={user.profileImage} />
               </div>
               <div className='summary'>
                 <div className='item'>
-                  <h4>my dub</h4><span className='semicolon'>:</span><div className='text-in-circle'>2</div>
+                  <h4>my dub</h4><span className='semicolon'>:</span><div className='text-in-circle'>{dubs.medias.length}</div>
                 </div>
                 <div className='item'>
-                  <h4>favorites</h4><span className='semicolon'>:</span><div className='text-in-circle'>3</div>
+                  <h4>favorites</h4><span className='semicolon'>:</span><div className='text-in-circle'>{favorites.medias.length}</div>
                 </div>
               </div>
             </div>
           </div>
-          <MediaSliderPanel data={favorites} />
-          <MediaSliderPanel data={dubs} />
+          <MediaSliderPanel data={[favorites]} />
+          <MediaSliderPanel data={[dubs]} />
           <Footer />
         </div>
       </div>
